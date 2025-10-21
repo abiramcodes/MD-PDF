478efecee09dd1911c6712df08aa683993b929b5
abiramcodes <codingkannan@gmail.com>
2025-10-21 18:50:51 +0530

    update: removed unnecessary files in BE

--- MD-PDF-BE/src/app.module.ts
+++ MD-PDF-BE/src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PdfModule } from './controllers/pdf/pdf.module';
 
 @Module({
   imports: [PdfModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
